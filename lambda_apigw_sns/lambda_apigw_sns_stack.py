839857246cd186f14575b7bb56c891676787f91d
Harsh Nagpal <harshnagpal@Harshs-MacBook-Pro.local>
2023-03-14 17:39:15 -0400

    added lambda

--- lambda_apigw_sns/lambda_apigw_sns_stack.py
+++ lambda_apigw_sns/lambda_apigw_sns_stack.py
@@ -1,6 +1,9 @@
 from aws_cdk import (
     # Duration,
     Stack,
+    aws_lambda as _lambda,
+    aws_sns as _sns,
+    aws_apigateway as apigw
     # aws_sqs as sqs,
 )
 from constructs import Construct
@@ -9,11 +12,12 @@ class LambdaApigwSnsStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
+        api_lambda = _lambda.Function(
+            self, 'apiLambda',
+            runtime=_lambda.Runtime.PYTHON_3_7,
+            code=_lambda.Code.from_asset('src'),
+            handler='apiLambda.handler',
+        )
 
-        # The code that defines your stack goes here
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "LambdaApigwSnsQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        
